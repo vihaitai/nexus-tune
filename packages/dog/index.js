4763e3a280acdcef1ceca2ccc5ca99992e1f8f67
huchaochao <huchaochao@witsky.cn>
2023-03-09 17:18:35 +0800

    fix

---
+++ packages/dog/index.js
@@ -0,0 +1,17 @@
+import chalk from 'chalk'
+
+export function add(a, b) {
+	return a + b
+}
+
+export format(str) {
+	return chalk.greeb(str)
+}
+
+export greet() {
+	console.log("cc sss yahaha hello world")
+}
+
+export where() {
+	console.log("witsky2023-03-09")
+}
